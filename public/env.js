b052c4b9fb8a1ce6653450f33748f6da5a372fb1
Chasham Shergill <chashamsarao@gmail.com>
2021-08-08 11:56:58 +0530

    > Modals and cards > Connecting to database

--- public/env.js
+++ public/env.js
@@ -1,22 +1,61 @@
-const testButtonFunction=()=>{
-    alert('Thank you for clicking')
-  }
-  
-  // connect to the socket
-  
-  let socket = io();
-  
-  
-  socket.on('number', (msg) => {
-      console.log('Random number: ' + msg);
+  const cardList = [
+    {
+        title: "Education",
+        image: "https://scholarship-positions.com/wp-content/uploads/2017/06/Deakin-University-Postgraduate-Research-Scholarship.png",
+        link: "Universities",
+        desciption: "This is DEakin university"
+    },
+    {
+        title: "Olympics 2021",
+        image: "https://images.indianexpress.com/2021/07/Untitled-design-6.jpg",
+        link: "News ",
+        desciption: "Here find more info."
+    }
+]
+
+const submitForm = () => {
+  let formData = {};
+  formData.title = $('#title').val();
+  formData.image = $('#image').val();
+  formData.link = $('#link').val();
+  formData.description = $('#description').val();
+
+  console.log("Project added successfully ", formData);
+  cardList.push(formData)
+  alert(result.message);
+  location.reload();
+}
+
+const addCards = (items) => {
+  items.forEach(item => {
+      let itemToAppend = '<div class="col s4 center-align">'+
+  '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+  '</div><div class="card-content">'+
+  '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+  '<div class="card-reveal">'+
+      '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+      '<p class="card-text">'+item.desciption+'</p>'+
+    '</div></div></div>';
+    $("#card-section").append(itemToAppend)
+  });
+}
+
+
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+  $('#formSubmit').click(()=>{
+      submitForm();
   })
+  addCards(cardList);
+  $('.modal').modal();
+});
+
   
   console.log('test')
   $(document).ready(function(){
     console.log('Ready')
-    
-    //bind the button
-    $('#testButton').click(testButtonFunction)
+
   
     //test get call
     $.get('/test?user_name="Fantastic User"',(result)=>{
--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 let express = require("express");
 let app = express();
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
 
-//var app = require('express')();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
-
-
-
-
-
-var port = process.env.PORT || 8080;
-
+const uri = 'mongodb+srv://sit725-prac4:<chasham123>@sit725-2021-t2-prac4.czepu.mongodb.net/sit725-week4?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {useNewUrlParser : true});
+var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+const createCollection = (collectionName) = {
+  client.connect((err,db) => {
+    projectCollection = client.db().collection(collectionName);
+    if (!err) {
+      console.log('Mongodb Connected')
+    }
+    else {
+    console.log("DB error", err);
+    process.exit(1);
+  }
+  )
+}
+}
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
 
-
-//socket test
-/*io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  }); 
-  setInterval(()=>{
-    socket.emit('number', parseInt(Math.random()*10));
-  }, 1000);
-
-}); */
-
-
-http.listen(port,()=>{
+app.listen(port,()=>{
   console.log("Listening on port ", port);
-});
-
-//this is only needed for Cloud foundry 
-// require("cf-deployment-tracker-client").track();
+  createCollection("Places")
+})
